020dbd48a588b12e734ad0c920d22c8b57c021e0
“larrycomb” <larrycomb@gmail.com>
2018-01-24 18:32:10 -0800

    1st saving

---
+++ slideTimer/ViewController.swift
@@ -0,0 +1,171 @@
+//
+//  ViewController.swift
+//  slideTimer
+//
+//  Created by LARRY COMBS on 1/21/18.
+//  Copyright © 2018 LARRY COMBS. All rights reserved.
+//
+
+import UIKit
+import AudioToolbox
+
+class ViewController: UIViewController, CountdownTimerDelegate {
+    
+    
+    
+    //MARK - Outlets
+    @IBOutlet weak var sliderHoursOutlet: UISlider!
+    
+    @IBAction func sliderHoursAction(_ sender: UISlider) {
+    }
+    @IBOutlet weak var sliderMinutesOutlet: UISlider!
+    @IBAction func sliderMinutesAction(_ sender: UISlider) {
+    }
+    @IBOutlet weak var sliderSecondsOutlet: UISlider!
+    @IBAction func sliderSecondsAction(_ sender: UISlider)
+    {
+        selectedSecs = Int(sender.value)
+        countdownTimer.duration = Double(sender.value)
+        seconds.text? = String(selectedSecs)
+        progressBar.timerDuration = self.selectedSecs
+    
+    }
+    @IBOutlet weak var Date: UILabel!
+    @IBOutlet weak var progressBar: ProgressBar!
+    @IBOutlet weak var hours: UILabel!
+    @IBOutlet weak var minutes: UILabel!
+    @IBOutlet weak var seconds: UILabel!
+    @IBOutlet weak var counterView: UIStackView!
+    @IBOutlet weak var stopBtn: UIButton!
+    @IBOutlet weak var startBtn: UIButton!
+    
+    
+    //MARK - Vars
+    
+    var countdownTimerDidStart = false
+    
+    lazy var countdownTimer: CountdownTimer = {
+        let countdownTimer = CountdownTimer()
+        return countdownTimer
+    }()
+    
+    
+    // Test, for dev
+    var selectedSecs = 10
+    let picker = UIDatePicker()
+    
+    
+    
+    lazy var messageLabel: UILabel = {
+        let label = UILabel(frame:CGRect.zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 24.0, weight: UIFont.Weight.light)
+        label.textColor = UIColor.white
+        label.textAlignment = .center
+        label.text = "Done!"
+        
+        
+        return label
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countdownTimer.delegate = self
+        countdownTimer.setTimer(hours: 0, minutes: 0, seconds: selectedSecs)
+        progressBar.setProgressBar(hours: 0, minutes: 0, seconds: selectedSecs)
+        stopBtn.isEnabled = false
+        stopBtn.alpha = 0.5
+        
+        view.addSubview(messageLabel)
+        
+        var constraintCenter = NSLayoutConstraint(item: messageLabel, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1, constant: 0)
+        self.view.addConstraint(constraintCenter)
+        constraintCenter = NSLayoutConstraint(item: messageLabel, attribute: .centerY, relatedBy: .equal, toItem: self.view, attribute: .centerY, multiplier: 1, constant: 0)
+        self.view.addConstraint(constraintCenter)
+        
+        
+        let date = NSDate()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE, MMMM dd, YYYY"
+        let stringDate = dateFormatter.string(from: date as Date)
+        Date.text = stringDate
+        
+        messageLabel.isHidden = true
+        counterView.isHidden = false
+    }
+    
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    
+    //MARK: - Countdown Timer Delegate
+    
+    func countdownTime(time: (hours: String, minutes: String, seconds: String)) {
+        hours.text = time.hours
+        minutes.text = time.minutes
+        seconds.text = time.seconds
+    }
+    
+    
+    func countdownTimerDone() {
+        
+        sliderSecondsOutlet.isHidden = false
+        counterView.isHidden = true
+        messageLabel.isHidden = false
+        seconds.text = String(selectedSecs)
+        countdownTimerDidStart = false
+        stopBtn.isEnabled = false
+        stopBtn.alpha = 0.5
+        startBtn.setTitle("START",for: .normal)
+        
+        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+        
+        print("countdownTimerDone")
+    }
+    
+    
+    //MARK: - Actions
+    
+    @IBAction func startTimer(_ sender: UIButton) {
+        
+        sliderSecondsOutlet.isHidden = true
+        messageLabel.isHidden = true
+        counterView.isHidden = false
+        
+        stopBtn.isEnabled = true
+        stopBtn.alpha = 1.0
+        
+        if !countdownTimerDidStart{
+            countdownTimer.start()
+            progressBar.start()
+            countdownTimerDidStart = true
+            startBtn.setTitle("PAUSE",for: .normal)
+            
+        }else{
+            countdownTimer.pause()
+            progressBar.pause()
+            countdownTimerDidStart = false
+            startBtn.setTitle("RESUME",for: .normal)
+        }
+    }
+    
+    
+    @IBAction func stopTimer(_ sender: UIButton) {
+        
+        sliderSecondsOutlet.isHidden = false
+        countdownTimer.stop()
+        progressBar.stop()
+        countdownTimerDidStart = false
+        stopBtn.isEnabled = false
+        stopBtn.alpha = 0.5
+        startBtn.setTitle("START",for: .normal)
+    }
+    
+    
+    
+    
+    
+}
